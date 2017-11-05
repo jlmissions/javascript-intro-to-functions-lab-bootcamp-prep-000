4a893076842735b164e5fb5fe45d9319c822a5f2
jlmissions@yahoo.com <jlmissions@yahoo.com>
2017-11-05 15:41:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,11 +14,3 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string){
-  if (string.toLowerCase()===string){
-    return "I can't hear you!"
-  }else if (string.toUpperCase()===string){
-    return "YES INDEED!"
-  }else (string==="I love you, Grandma.")
-    return "I love you, too."
-  }
